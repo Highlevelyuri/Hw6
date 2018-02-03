9f3e04b81e274bc6f49164c122496f7fd75cec78
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 09:48:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2, b = 8, h=0;
+        do {
+            h+=2;
+            System.out.println( h  + "\t" + (h - Math.sin(h)));
+        }
+        while (h<8);
+    }
+ }
+
+
+
+
